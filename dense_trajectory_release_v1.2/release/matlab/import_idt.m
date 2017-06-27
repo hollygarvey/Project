be133661c05bf017d97acdb0f0bcbbe486c21305
hollygarvey <hollygarvey@gmail.com>
2017-06-27 15:59:09 +0100

    new import

--- dense_trajectory_release_v1.2/release/matlab/import_idt.m
+++ dense_trajectory_release_v1.2/release/matlab/import_idt.m
@@ -5,20 +5,20 @@ function feature = import_idt (file, tra_len, l)
 %       tra_len: the length of improved trajectories (default: 15)
 % Output:
 %       feature:  imported idt features
-    index = randperm(size(descriptors.label(1,:),2));
     fid = fopen(file,'rb');
     feat = fread(fid,[10+4*tra_len+96*3+108,inf],'float');
-	feature = struct('label',[],'info',[],'tra',[],'tra_shape',[],'hog',[],'hof',[],'mbhx',[],'mbhy',[]);
+    index = randperm(size(feat(1,:),2));
+	feature = struct('label',zeros(1,6666),'info',zeros(10,6666),'tra',zeros(30,6666),'tra_shape',zeros(30,6666),'hog',zeros(96,6666),'hof',zeros(108,6666),'mbhx',zeros(96,6666),'mbhy',zeros(96,6666));
 	for i=1:6666
-        feature.label = l;
-		feature.info = feat(1:10,index(i));
-		feature.tra = feat(11:10+tra_len*2,index(i));
-                feature.tra_shape = feat(11+tra_len*2:10+tra_len*4,index(i));
+        feature.label(i) = l;
+		feature.info(:,i) = feat(1:10,index(i));
+		feature.tra(:,i) = feat(11:10+tra_len*2,index(i));
+                feature.tra_shape(:,i) = feat(11+tra_len*2:10+tra_len*4,index(i));
                 ind = 10+tra_len*4;
-		feature.hog = feat(ind+1:ind+96,index(i));
-		feature.hof = feat(ind+97:ind+204,index(i));
-		feature.mbhx = feat(ind+205:ind+300,index(i));
-		feature.mbhy = feat(ind+301:end,index(i));
+		feature.hog(:,i) = feat(ind+1:ind+96,index(i));
+		feature.hof(:,i) = feat(ind+97:ind+204,index(i));
+		feature.mbhx(:,i) = feat(ind+205:ind+300,index(i));
+		feature.mbhy(:,i) = feat(ind+301:end,index(i));
 	end
     fclose(fid);
 end
