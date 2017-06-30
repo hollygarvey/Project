049b4991556dc4ac039f5c36bbe8ef63ed508d2f
hollygarvey <hollygarvey@gmail.com>
2017-06-30 13:27:38 +0100

    more formatting

--- dense_trajectory_release_v1.2/release/matlab/format_data.m
+++ dense_trajectory_release_v1.2/release/matlab/format_data.m
@@ -29,12 +29,13 @@
 % stand_data = import_idt('out.features.stand.gz',15);
 % turn_data = import_idt('out.features.turn.gz',15);
 % walk_data = import_idt('out.features.walk.gz',15);
+clear;
 
 direct = '/Users/holly/Documents/Project/dense_trajectory_release_v1.2/release/matlab/data/jpl';
 files = dir(direct);
 
 fileIndex = find(~[files.isdir]);
-data = struct('name',{},'info',{},'tra',{},'tra_shape',{},'hog',{},'hof',{},'mbhx',{},'mbhy',{});
+data = struct('name',{},'hog',{});
 
 for i = 1:length(fileIndex)
     fileName = files(fileIndex(i)).name;
@@ -50,40 +51,63 @@ save('final_data')
 %Want dictionary size 4000
 
 %dict_size = 4000;
-feat_size = 100000;
-%each_class = feat_size/8;
-
-descriptors = struct('label',[ones(jump_size) 2*ones(run_size) 3*ones(sit_size)],...
-                     'info',[jump_data.info run_data.info sit_data.info],...
-                     'tra',[jump_data.tra run_data.tra sit_data.tra],...
-                     'tra_shape',[jump_data.tra_shape run_data.tra_shape sit_data.tra_shape],...
-                     'hog',[jump_data.hog run_data.hog sit_data.hog],...
-                     'hof',[jump_data.hof run_data.hof sit_data.hof],...
-                     'mbhx',[jump_data.mbhx run_data.mbhx sit_data.mbhx],...
-                     'mbhy',[jump_data.mbhy run_data.mbhy sit_data.mbhy]);
+% feat_size = 100000;
+% 
+for i=1:size(data,1)
+    if i==1
+        descriptors = struct('name', repmat(string(data(1).name),1,size(data(1).hog,2)),'hog',data(1).hog);
+    else
+        descriptors.name = [descriptors.name repmat(string(data(i).name),1,size(data(i).hog,2))];
+        descriptors.hog = [descriptors.hog data(i).hog];
+    end
+end
+%  descriptors = struct('label',[ones(jump_size) 2*ones(run_size) 3*ones(sit_size)],...
+%                      'info',[jump_data.info run_data.info sit_data.info],...
+%                      'tra',[jump_data.tra run_data.tra sit_data.tra],...
+%                      'tra_shape',[jump_data.tra_shape run_data.tra_shape sit_data.tra_shape],...
+%                      'hog',[jump_data.hog run_data.hog sit_data.hog],...
+%                      'hof',[jump_data.hof run_data.hof sit_data.hof],...
+%                      'mbhx',[jump_data.mbhx run_data.mbhx sit_data.mbhx],...
+%                      'mbhy',[jump_data.mbhy run_data.mbhy sit_data.mbhy]);
 
 %%
 %Choose number of descriptors
-descriptors_chosen = struct('label',[zeros(1,feat_size)],...
-                     'info',[zeros(10,feat_size)],...
-                     'tra',[zeros(30,feat_size)],...
-                     'tra_shape',[zeros(30,feat_size)],...
-                     'hog',[zeros(96,feat_size)],...
-                     'hof',[zeros(108,feat_size)],...
-                     'mbhx',[zeros(96,feat_size)],...
-                     'mbhy',[zeros(96,feat_size)]);
+feat_size = 100000;
 
-index = randperm(size(descriptors.label(1,:),2));
-for i=1:feat_size
-    j = index(i);
-    descriptors_chosen.label(1,i) = descriptors.label(1,j);
-    descriptors_chosen.info(10,i) = descriptors.info(10,j);
-    descriptors_chosen.tra(30,i) = descriptors.tra(30,j);
-    descriptors_chosen.tra_shape(30,i) = descriptors.tra_shape(30,j);
-    descriptors_chosen.hog(96,i) = descriptors.hog(96,j);
-    descriptors_chosen.hof(108,i) = descriptors.hof(108,j);
-    descriptors_chosen.mbhx(96,i) = descriptors.mbhx(96,j);
-    descriptors_chosen.mbhy(96,i) = descriptors.mbhy(96,j);
-end
+descriptors_chosen = struct('name',repmat(string(0),1,feat_size),'hog',[zeros(96,feat_size)]);
+%                      'info',[zeros(10,feat_size)],...
+%                      'tra',[zeros(30,feat_size)],...
+%                      'tra_shape',[zeros(30,feat_size)],...
+%                      'hog',[zeros(96,feat_size)],...
+%                      'hof',[zeros(108,feat_size)],...
+%                      'mbhx',[zeros(96,feat_size)],...
+%                      'mbhy',[zeros(96,feat_size)]
 
-save('descriptors_chosen');
+% total =0;
+% for i = 1:size(data,1)
+%     total = total + size(data(i).hog,2);
+% end
+% 
+% av = feat_size/total;
+% 
+% di=0;
+
+% for k = 1:size(data,1)
+    index = randperm(size(descriptors.hog,2));
+%     name = data(k).name;
+%     temp = uint16(av*size(data(k).hog,2));
+    for i=1:feat_size
+        j = index(i);
+        descriptors_chosen.name(i) = descriptors.name(j);
+%         descriptors_chosen.info(:,i+di) = data(k).info(:,j);
+%         descriptors_chosen.tra(:,i+di) = data(k).tra(:,j);
+%         descriptors_chosen.tra_shape(:,i+di) = data(k).tra_shape(:,j);
+        descriptors_chosen.hog(:,i) = descriptors.hog(:,j);
+%         descriptors_chosen.hof(:,i+di) = data(k).hof(:,j);
+%         descriptors_chosen.mbhx(:,i+di) = data(k).mbhx(:,j);
+%         descriptors_chosen.mbhy(:,i+di) = data(k).mbhy(:,j);
+    end
+%     di = di+temp;
+% end
+
+% save('descriptors_chosen','descriptors_chosen');
--- dense_trajectory_release_v1.2/release/matlab/import_idt1.m
+++ dense_trajectory_release_v1.2/release/matlab/import_idt1.m
@@ -8,17 +8,17 @@ function feature = import_idt (file, tra_len,name)
 
     fid = fopen(file,'rb');
     feat = fread(fid,[10+4*tra_len+96*3+108,inf],'float');
-	feature = struct('name',[],'info',[],'tra',[],'tra_shape',[],'hog',[],'hof',[],'mbhx',[],'mbhy',[]);
+	feature = struct('name',[],'hog',[]);
 	if ~isempty(feat)
         feature.name = name;
-		feature.info = feat(1:10,:);
-		feature.tra = feat(11:10+tra_len*2,:);
-                feature.tra_shape = feat(11+tra_len*2:10+tra_len*4,:);
+% 		feature.info = feat(1:10,:);
+% 		feature.tra = feat(11:10+tra_len*2,:);
+%                 feature.tra_shape = feat(11+tra_len*2:10+tra_len*4,:);
                 ind = 10+tra_len*4;
 		feature.hog = feat(ind+1:ind+96,:);
-		feature.hof = feat(ind+97:ind+204,:);
-		feature.mbhx = feat(ind+205:ind+300,:);
-		feature.mbhy = feat(ind+301:end,:);
+% 		feature.hof = feat(ind+97:ind+204,:);
+% 		feature.mbhx = feat(ind+205:ind+300,:);
+% 		feature.mbhy = feat(ind+301:end,:);
 	end
     fclose(fid);
 end
